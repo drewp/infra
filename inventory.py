880a1380fd1088d69b645478debafd04ec5f02b5
drewp@bigasterisk.com <drewp@bigasterisk.com>
2021-12-28 22:03:50 -0800

    some host and address updates

--- inventory.py
+++ inventory.py
@@ -4,36 +4,37 @@ ssh_key = '/root/.ssh/id_ecdsa'
 big = [
     ('bang', {
         'wireguard_address': '10.5.0.1',
-    }),
+    }),  #
     ('dash', {
+        "mac": "b8:97:5a:17:d7:1f",
         'ssh_hostname': '10.1.0.5',
-        'interface': 'enp2s0',
         'addr': '10.1.0.5',
         'wireguard_address': '10.5.0.5',
     }),
     ('slash', {
+        'mac': 'c8:60:00:98:ec:74',
         'ssh_hostname': '10.1.0.6',
-        'interface': 'enp3s0',
         'addr': '10.1.0.6',
         'wireguard_address': '10.5.0.6',
     }),
     ('dot', {
-        'ssh_hostname': '10.2.0.57',
         'interface': 'enp3s0',
+        'ssh_hostname': '10.2.0.69',
         'addr': '10.2.0.30',
         'wireguard_address': '10.5.0.30',
     })
 ]
+
 pi = [
     ('frontbed', {
+        'mac': 'b8:27:eb:e9:d3:44',
         'ssh_hostname': '10.2.0.17',
-        'interface': 'eth0',
         'addr': '10.2.0.17',
         'wireguard_address': '10.5.0.17',
     }),
     ('garage', {
-        'ssh_hostname': '10.2.0.19',
-        'interface': 'eth0',
+        'mac': 'b8:27:eb:81:17:92',
+        'ssh_hostname': '10.2.0.14',
         'addr': '10.2.0.14',
         'wireguard_address': '10.5.0.14',
     }),
@@ -41,7 +42,13 @@ pi = [
 
 remote = [
     ('prime', {
+        'mac': '04:01:09:7f:89:01',
         'ssh_hostname': 'public.bigasterisk.com',
+        'addr': '162.243.138.136',
         'wireguard_address': '10.5.0.2',
+    }),
+    ('plus', {
+        'ssh_hostname': '10.2.0.110',
+        'wireguard_address': '10.5.0.110',
     }),
 ]
