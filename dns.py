3df445e5c886060b4758fd7224b0e89e50961845
drewp@bigasterisk.com <drewp@bigasterisk.com>
2022-07-10 19:50:52 -0700

    reactor and temporarily cut dep on lanscape

--- dns.py
+++ dns.py
@@ -7,6 +7,8 @@ from pyinfra.operations import apt, files, server, systemd
 
 
 def prepare_dhcp_hosts():
+    empty = tempfile.NamedTemporaryFile(mode='wt')
+    return empty
     lanscape_ip = subprocess.check_output([
         'kubectl',
         'get',
@@ -37,6 +39,19 @@ def resolv_conf_static_file():
                 ns='10.5.0.1' if host.name in ['prime', 'plus'] else '10.2.0.1',
                 force=True)
 
+def dnsmasq_instance(dhcp_hosts, net_name):
+    files.directory(path=f'/opt/dnsmasq/{net_name}')
+    files.template(src='templates/dnsmasq/dnsmasq.conf.j2', dest=f'/opt/dnsmasq/{net_name}/dnsmasq.conf', net=net_name)
+    files.template(src='templates/dnsmasq/hosts.j2', dest=f'/opt/dnsmasq/{net_name}/hosts', net=net_name)
+    files.template(src=dhcp_hosts.name, dest=f'/opt/dnsmasq/{net_name}/dhcp_hosts', net=net_name)
+
+    files.template(src='templates/dnsmasq/dnsmasq.service.j2',
+                   dest=f'/etc/systemd/system/dnsmasq_{net_name}.service',
+                   net=net_name)
+    if net_name == '10.2':
+        systemd.service(service=f'dnsmasq_{net_name}', enabled=True, restarted=True, daemon_reload=True)
+        # 10.5 is after wireguard setup
+    
 if host.name == 'bang':
     resolv_conf_static_file()
     apt.packages(packages=['dnsmasq'])
@@ -45,17 +60,10 @@ if host.name == 'bang':
 
     dhcp_hosts = prepare_dhcp_hosts()
 
-    for net_name in ['10.2', '10.5']:
-        files.directory(path=f'/opt/dnsmasq/{net_name}')
-        files.template(src='templates/dnsmasq/dnsmasq.conf.j2', dest=f'/opt/dnsmasq/{net_name}/dnsmasq.conf', net=net_name)
-        files.template(src='templates/dnsmasq/hosts.j2', dest=f'/opt/dnsmasq/{net_name}/hosts', net=net_name)
-        files.template(src=dhcp_hosts.name, dest=f'/opt/dnsmasq/{net_name}/dhcp_hosts', net=net_name)
+    dnsmasq_instance(dhcp_hosts, '10.2')
+    dnsmasq_instance(dhcp_hosts, '10.5') # only works after wireguard is up
 
-        files.template(src='templates/dnsmasq/dnsmasq.service.j2',
-                       dest=f'/etc/systemd/system/dnsmasq_{net_name}.service',
-                       net=net_name)
-        systemd.service(service=f'dnsmasq_{net_name}', enabled=True, restarted=True, daemon_reload=True)
-
+        
 if host.name in [
         'garage',
         'dash',
