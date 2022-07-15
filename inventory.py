a01d40fd1682126208ed61e0a1a5c2fd5f3ef4d3
drewp@bigasterisk.com <drewp@bigasterisk.com>
2022-07-15 14:25:58 -0700

    usual host/addr churn

--- inventory.py
+++ inventory.py
@@ -6,15 +6,15 @@ big = [
         'wireguard_address': '10.5.0.1',
     }),  #
     ('dash', {
-        'ssh_hostname': '10.2.0.210',
+        'ssh_hostname': '10.2.0.23',
         'wireguard_address': '10.5.0.5',
     }),
     ('slash', {
-        'ssh_hostname': 'slash',
+        'ssh_hostname': '10.2.0.82',
         'wireguard_address': '10.5.0.6',
     }),
     ('dot', {
-        'ssh_hostname': 'dot',
+        'ssh_hostname': '10.2.0.47',
         'wireguard_address': '10.5.0.30',
     })
 ]
@@ -27,14 +27,14 @@ small = [
 ]
 
 pi = [
-#    ('frontbed', {
-#        'mac': 'b8:27:eb:e9:d3:44',
-#        'ssh_hostname': 'frontbed',
-#        'wireguard_address': '10.5.0.17',
-#    }),
+   ('frontbed', {
+       'mac': 'b8:27:eb:e9:d3:44',
+       'ssh_hostname': '10.2.0.27',
+       'wireguard_address': '10.5.0.17',
+   }),
     ('garage', {
         'mac': 'b8:27:eb:81:17:92',
-        'ssh_hostname': 'garage',
+        'ssh_hostname': '10.2.0.61',
         'wireguard_address': '10.5.0.14',
     }),
 ]
@@ -46,7 +46,7 @@ remote = [
         'wireguard_address': '10.5.0.2',
     }),
     ('plus', {
-        'ssh_hostname': 'plus',
+        'ssh_hostname': '10.2.0.40',
         'wireguard_address': '10.5.0.110',
     }),
 ]
